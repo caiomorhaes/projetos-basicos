b2e4ff966f1596fb8923d5ac589f70f0eb5ff81b
Caio Moraes <caiomorhaes@gmail.com>
2026-02-06 22:05:53 -0300

    Update geladeira.py
    atualizei o programa para incluir comentarios explicativos, um sistema anti missinput do usuario, onde o codigo não avança a não ser que insira numeros na seleção de geladeira, alem de incluir uma forma de evitar numeros acima ou abaixo da quantidade de itens na lista

--- codigos meus/geladeira.py
+++ codigos meus/geladeira.py
@@ -1,23 +1,61 @@
+#introdução
 print (' Você acabou de abrir a sua geladeira, nela você busca uma bebida, uma comida e uma sobremesa')
+#listas
 bebida = ['coca','guarana','h20','suco natural']
 comida = ['macarrao','arroz e ovo','lasanha','hamburguer']
 sobremesa = ['sorvete','açai','bolo','fruta']
 print('                    ')
+#estabelecer uma logica para o while
 resultado1 = 'N'
 while resultado1 != 'S':
-    print(bebida)
-    selecao_bebida = input ('Selecione uma bebida para começar(0-3): ')
-    selecao_bebida = int(selecao_bebida)
+    #evitar que o usuario coloque letras no input
+    while True:
+        print(bebida)
+        selecao_bebida = (input ('Selecione uma bebida para começar(0-3): '))
+        if not (selecao_bebida.isdigit()):
+            print('                    ')
+            print("-" * 30)
+            print('por favor só insira números no terminal')
+            print("-" * 30)
+            print('                    ')
+            continue
+        selecao_bebida = int(selecao_bebida)
+        if not selecao_bebida <= 3 and selecao_bebida >= 0:
+            print('                    ')
+            print("-" * 30)
+            print('não insira números alem ou abaixo da quantidade de números na lista ')
+            print("-" * 30)
+            print('                    ')
+            continue
+        break
+    #formatar para int para ler na lista
     print('                    ')
+    #enfase no .upper para evitar que o usuario coloque a resposta em minusculo
     resultado1 = input ('você selecionou ' + bebida[selecao_bebida] + '? [S,N]  ').upper()
     print('                    ')
     a = bebida[selecao_bebida]
 print ('Agora para a comida')
 resultado2 = 'N'
 while resultado2 != 'S':
-    print(comida)
-    selecao_comida = input ('Selecione uma comida(0-3) ')
-    selecao_comida = int(selecao_comida)
+    while True:
+        print(comida)
+        selecao_comida = input ('Selecione uma comida(0-3) ')
+        if not (selecao_comida.isdigit()):
+            print('                    ')
+            print("-" * 30)
+            print('por favor só insira numeros no terminal')
+            print("-" * 30)
+            print('                    ')
+            continue
+        selecao_comida = int(selecao_comida)
+        if not selecao_comida <= 3 and selecao_comida >= 0:
+            print('                    ')
+            print("-" * 30)
+            print('não insira números alem ou abaixo da quantidade de números na lista ')
+            print("-" * 30)
+            print('                    ')
+            continue
+        break
     print ('                    ')
     resultado2 = input ('você selecionou ' + comida[selecao_comida] + '? [S,N]  ').upper()
     print('                    ')
@@ -25,12 +63,29 @@ while resultado2 != 'S':
 print ('Agora para a sobremesa')
 resultado3 = 'N'
 while resultado3 != 'S':
-    print(sobremesa)
-    selecao_sobremesa = input ('Selecione uma sobremesa(0-3): ')
-    selecao_sobremesa = int(selecao_sobremesa)
+    while True:
+        print(sobremesa)
+        selecao_sobremesa = input ('Selecione uma sobremesa(0-3): ')
+        if not (selecao_sobremesa.isdigit()):
+            print('                    ')
+            print("-" * 30)
+            print('por favor só insira numeros no terminal')
+            print("-" * 30)
+            print('                    ')
+            continue
+        selecao_sobremesa = int(selecao_sobremesa)
+        if not selecao_sobremesa <= 3 and selecao_sobremesa >= 0:
+            print('                    ')
+            print("-" * 30)
+            print('não insira números alem ou abaixo da quantidade de números na lista ')
+            print("-" * 30)
+            print('                    ')
+            continue
+        break
     print('                    ')
     resultado3 = input ('você selecionou ' + sobremesa[selecao_sobremesa] + '? [S,N]  ').upper()
     print('                    ')
     c = sobremesa[selecao_sobremesa]
 print (f'Parabens, no fim você selecionou {a} para beber, {b} para comer e {c} para sobremesa')
+
 
